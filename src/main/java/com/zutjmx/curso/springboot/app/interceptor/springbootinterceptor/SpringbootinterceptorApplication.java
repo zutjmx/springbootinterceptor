3ff62185a6cbf5b22198eafb8968cafebeb446c5
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2025-01-08 13:54:10 -0600

    first commit

---
+++ src/main/java/com/zutjmx/curso/springboot/app/interceptor/springbootinterceptor/SpringbootinterceptorApplication.java
@@ -0,0 +1,13 @@
+package com.zutjmx.curso.springboot.app.interceptor.springbootinterceptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootinterceptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootinterceptorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zutjmx/curso/springboot/app/interceptor/springbootinterceptor/SpringbootinterceptorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zutjmx.curso.springboot.app.interceptor.springbootinterceptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootinterceptorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
